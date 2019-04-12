7be3b00556f7cce9bf3b9ca94f3c9b958fa141a3
Allan Gonzalez <allangonza04@gmail.com>
2019-04-11 21:03:37 -0600

    Set to DB first

--- TallerBilly/TallerBilly/Controllers/CreateCarModelsController.cs
+++ TallerBilly/TallerBilly/Controllers/CreateCarModelsController.cs
@@ -10,119 +10,107 @@ using TallerBilly.Models;
 
 namespace TallerBilly.Controllers
 {
-    public class CreateCarModelsController : Controller
+    public class createcarmodelsController : Controller
     {
-        public TallerBillyDB db = new TallerBillyDB();
+        private TallerBillyEntities1 db = new TallerBillyEntities1();
 
-        // GET: CreateCarModels
+        // GET: createcarmodels
         public ActionResult Index()
         {
-            return View(db.Cars.ToList());
+            return View(db.createcarmodels.ToList());
         }
 
-        // GET: CreateCarModels/Details/5
+        // GET: createcarmodels/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreateCarModel createCarModel = db.Cars.Find(id);
-            if (createCarModel == null)
+            createcarmodel createcarmodel = db.createcarmodels.Find(id);
+            if (createcarmodel == null)
             {
                 return HttpNotFound();
             }
-            return View(createCarModel);
+            return View(createcarmodel);
         }
 
-        //public ActionResult getUsers()
-        //{
-        //    //return Json(db.Users.Select(x => new
-        //    //{
-        //    //    ID = x.Id,
-        //    //}).ToList(), JsonRequestBehavior.AllowGet);
-        //    return View();
-        //}
-
-        // GET: CreateCarModels/Create
+        // GET: createcarmodels/Create
         public ActionResult Create()
         {
-            ViewBag.users = db.Users.ToList();
-
             return View();
         }
 
-        // POST: CreateCarModels/Create
+        // POST: createcarmodels/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Placa,VIN,Modelo,Marca,Ano,Combustible,Transmision")] CreateCarModel createCarModel)
+        public ActionResult Create([Bind(Include = "Id,Placa,VIN,Modelo,Marca,Ano,Combustible,Transmision")] createcarmodel createcarmodel)
         {
             if (ModelState.IsValid)
             {
-                db.Cars.Add(createCarModel);
+                db.createcarmodels.Add(createcarmodel);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(createCarModel);
+            return View(createcarmodel);
         }
 
-        // GET: CreateCarModels/Edit/5
+        // GET: createcarmodels/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreateCarModel createCarModel = db.Cars.Find(id);
-            if (createCarModel == null)
+            createcarmodel createcarmodel = db.createcarmodels.Find(id);
+            if (createcarmodel == null)
             {
                 return HttpNotFound();
             }
-            return View(createCarModel);
+            return View(createcarmodel);
         }
 
-        // POST: CreateCarModels/Edit/5
+        // POST: createcarmodels/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Placa,VIN,Modelo,Marca,Ano,Combustible,Transmision")] CreateCarModel createCarModel)
+        public ActionResult Edit([Bind(Include = "Id,Placa,VIN,Modelo,Marca,Ano,Combustible,Transmision")] createcarmodel createcarmodel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(createCarModel).State = EntityState.Modified;
+                db.Entry(createcarmodel).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(createCarModel);
+            return View(createcarmodel);
         }
 
-        // GET: CreateCarModels/Delete/5
+        // GET: createcarmodels/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreateCarModel createCarModel = db.Cars.Find(id);
-            if (createCarModel == null)
+            createcarmodel createcarmodel = db.createcarmodels.Find(id);
+            if (createcarmodel == null)
             {
                 return HttpNotFound();
             }
-            return View(createCarModel);
+            return View(createcarmodel);
         }
 
-        // POST: CreateCarModels/Delete/5
+        // POST: createcarmodels/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            CreateCarModel createCarModel = db.Cars.Find(id);
-            db.Cars.Remove(createCarModel);
+            createcarmodel createcarmodel = db.createcarmodels.Find(id);
+            db.createcarmodels.Remove(createcarmodel);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- TallerBilly/TallerBilly/Controllers/createusermodelsController.cs
+++ TallerBilly/TallerBilly/Controllers/createusermodelsController.cs
@@ -10,107 +10,107 @@ using TallerBilly.Models;
 
 namespace TallerBilly.Controllers
 {
-    public class CreateUserController : Controller
+    public class createusermodelsController : Controller
     {
-        private TallerBillyDB db = new TallerBillyDB();
+        private TallerBillyEntities1 db = new TallerBillyEntities1();
 
-        // GET: CreateUser
+        // GET: createusermodels
         public ActionResult Index()
         {
-            return View(db.Users.ToList());
+            return View(db.createusermodels.ToList());
         }
 
-        // GET: CreateUser/Details/5
+        // GET: createusermodels/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreateUserModel createUserModel = db.Users.Find(id);
-            if (createUserModel == null)
+            createusermodel createusermodel = db.createusermodels.Find(id);
+            if (createusermodel == null)
             {
                 return HttpNotFound();
             }
-            return View(createUserModel);
+            return View(createusermodel);
         }
 
-        // GET: CreateUser/Create
+        // GET: createusermodels/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: CreateUser/Create
+        // POST: createusermodels/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nombre,Apellido,Cedula,Email,Contacto,Direccion")] CreateUserModel createUserModel)
+        public ActionResult Create([Bind(Include = "Id,Nombre,Apellido,Cedula,Email,Contacto,Direccion")] createusermodel createusermodel)
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(createUserModel);
+                db.createusermodels.Add(createusermodel);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(createUserModel);
+            return View(createusermodel);
         }
 
-        // GET: CreateUser/Edit/5
+        // GET: createusermodels/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreateUserModel createUserModel = db.Users.Find(id);
-            if (createUserModel == null)
+            createusermodel createusermodel = db.createusermodels.Find(id);
+            if (createusermodel == null)
             {
                 return HttpNotFound();
             }
-            return View(createUserModel);
+            return View(createusermodel);
         }
 
-        // POST: CreateUser/Edit/5
+        // POST: createusermodels/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nombre,Apellido,Cedula,Email,Contacto,Direccion")] CreateUserModel createUserModel)
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Apellido,Cedula,Email,Contacto,Direccion")] createusermodel createusermodel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(createUserModel).State = EntityState.Modified;
+                db.Entry(createusermodel).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(createUserModel);
+            return View(createusermodel);
         }
 
-        // GET: CreateUser/Delete/5
+        // GET: createusermodels/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreateUserModel createUserModel = db.Users.Find(id);
-            if (createUserModel == null)
+            createusermodel createusermodel = db.createusermodels.Find(id);
+            if (createusermodel == null)
             {
                 return HttpNotFound();
             }
-            return View(createUserModel);
+            return View(createusermodel);
         }
 
-        // POST: CreateUser/Delete/5
+        // POST: createusermodels/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            CreateUserModel createUserModel = db.Users.Find(id);
-            db.Users.Remove(createUserModel);
+            createusermodel createusermodel = db.createusermodels.Find(id);
+            db.createusermodels.Remove(createusermodel);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
